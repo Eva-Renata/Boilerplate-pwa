346019792b3550d4855edf98787d2b2c1209c6bb
Eva-Renata <all2evi@yahoo.com>
2022-02-08 14:00:52 +0100

    cache metods

---
+++ index.js
@@ -0,0 +1,20 @@
+// serviceworker registration
+
+//hvis der er en  "serviceWorker" i browseren, så registeres(importes) js
+//navigator = browser
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("serviceworker.js")
+
+    .then (registration => {
+        console.log("SW registered");
+        console.log(registration)
+    }).catch(error => {
+        console.log("SW register failed");
+        console.log(error)
+    })
+
+} else {
+    //browser does not support service worker
+    alert("PWA not supported");
+    //you can do non PWA stuff here
+}
---
+++ serviceworker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'my-site-cache';
+
+const urlsToCache = [
+    './',
+    './index.css',
+    'assets/images/icons/icon512.png',
+    './index.html', 
+    './info.html'
+];
+
+//cache kører en gang når jeg installerer appen
+
+self.addEventListener('install', function(event) {
+    //perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+
+        .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    )
+})
+
+self.addEventListener('fetch', function(e){
+    console.log('intercept req:' + e.request.url);
+
+    //caching strategies goes here
+})
